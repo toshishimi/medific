452cf14bba4fb7d205432ec6f2938fe16ae95878
toshishimi <sc.104.k@gmail.com>
2023-09-22 22:58:05 +0900

    初期設定を変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()  // 画面遷移を高速化するための記述
 require("@rails/activestorage").start()
 require("channels")
 
