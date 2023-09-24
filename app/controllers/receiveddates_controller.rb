16db496fa46b930a8fc5a53cbbe96ead5bda09e8
toshishimi <sc.104.k@gmail.com>
2023-09-24 22:22:24 +0900

    簡易的なビューを作成

--- app/controllers/receiveddates_controller.rb
+++ app/controllers/receiveddates_controller.rb
@@ -1,4 +1,15 @@
 class ReceiveddatesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "receiveddates#index"
-  
+  resources :receiveddates, only: [:index, :new, :create]
 end
