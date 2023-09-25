952ef9d52ad7db9aaac66c6f199df6d11d7f6b71
toshishimi <sc.104.k@gmail.com>
2023-09-26 02:48:26 +0900

    ルーティング調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "receiveddates#index"
-  resources :receiveddates, only: [:index, :new, :create]
+  root to: "received_dates#index"
+  resources :received_dates, only: [:index, :new, :create]
 end
