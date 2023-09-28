168b0cc1eeff5df27935990a121923651405178d
toshishimi <sc.104.k@gmail.com>
2023-09-28 22:54:12 +0900

    テスト変更

--- spec/models/total_info_spec.rb
+++ spec/models/total_info_spec.rb
@@ -8,53 +8,56 @@ RSpec.describe TotalInfo, type: :model do
   end
 
 
-  describe 'お薬登録' do
-    context '新規登録できる場合' do
+  describe "お薬登録" do
+    context "新規登録できる場合" do
       it "すべての項目が存在すれば登録できる" do
         expect(@total_info).to be_valid
       end
-      it "user_idが空でなければ保存できる" do
+      it "user_idが空でなければ登録できる" do
         @total_info.user_id = 1
         expect(@total_info).to be_valid
       end
-      it "タイミングが空欄でも保存できる" do
+      it "タイミングが空欄でも登録できる" do
         @total_info.timing = nil
         expect(@total_info).to be_valid
       end
-      it "回数が空欄でも保存できる" do
+      it "回数が空欄でも登録できる" do
         @total_info.individual = nil
         expect(@total_info).to be_valid
       end
-      it "何日分が空欄でも保存できる" do
+      it "何日分が空欄でも登録できる" do
         @total_info.days_supply = nil
         expect(@total_info).to be_valid
       end
-      it "備考が空欄でも保存できる" do
+      it "備考が空欄でも登録できる" do
         @total_info.notes = nil
         expect(@total_info).to be_valid
       end
     end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    context "新規登録できない場合" do
+      it "薬をもらった日が空だと登録できない" do
+        @total_info.date = nil
+        @total_info.valid?
+        expect(@total_info.errors.full_messages).to include("Date can't be blank")
+      end
+      it "病院名が空だと登録できない" do
+        @total_info.hospital_name = nil
+        @total_info.valid?
+        expect(@total_info.errors.full_messages).to include("Hospital name can't be blank")
+      end
+      it "薬名が空だと登録できない" do
+        @total_info.medicine_name = nil
+        @total_info.valid?
+        expect(@total_info.errors.full_messages).to include("Medicine name can't be blank")
+      end
+      it "user_idが空だと登録できない" do
+        @total_info.user_id = nil
+        binding.pry
+        @total_info.valid?
+        expect(@total_info.errors.full_messages).to include("User can't be blank")
+      end
+    end
 
 
   end
